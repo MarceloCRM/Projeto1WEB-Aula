d6708273a74f1b03f8ffa38aaa94bf39225291fe
MarceloCRM <marcelo.crmoura@gmail.com>
2024-10-22 20:33:05 -0300

    Banco e dados

---
+++ Apoio/Controllers/ApoioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Apoio.Controllers
+{
+    public class ApoioController : Controller
+    {
+        public IActionResult Empresa()
+        {
+            return View();
+        }
+    }
+}
---
+++ Apoio/Models/Aposta.cs
@@ -0,0 +1,12 @@
+﻿namespace Apoio.Models
+{
+    public class Aposta
+    {
+        public int Id { get; set; }
+        public decimal Valor { get; set; }
+        public decimal PrevisaoDeGanho { get; set; }
+        public bool Vencedor { get; set; }
+        public int ApostadorId { get; set; }
+        public Apostador? Apostador { get; set; }
+    }
+}
