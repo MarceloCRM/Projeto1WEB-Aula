49b7abb056a331239be3740645ab974e53da4b5a
MarceloCRM <marcelo.crmoura@gmail.com>
2024-11-05 20:35:09 -0300

    Create e List do Apostas

---
+++ Apoio/Controllers/ApostaController.cs
@@ -0,0 +1,48 @@
+﻿using Apoio.Data;
+using Apoio.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Apoio.Controllers
+{
+    public class ApostaController : Controller
+    {
+        private readonly WebetContext _context;
+
+        public ApostaController(WebetContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var apostaContext = _context.Apostas.Include(ap => ap.Apostador);
+            return View(apostaContext.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["ApostadorId"] = new SelectList(_context.Apostadores, "Id", "Nome");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Valor", "PrevisaoDeGanho", "Vencedora", "ApostadorId")] Aposta aposta)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["ApostadorId"] = new SelectList(_context.Apostadores, "Id", "Nome", aposta.Id);
+                return View(aposta);
+            }
+            if (aposta == null)
+            {
+                return NotFound();
+            }
+            _context.Add(aposta);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
